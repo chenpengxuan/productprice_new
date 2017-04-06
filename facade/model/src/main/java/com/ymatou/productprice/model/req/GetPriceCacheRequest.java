413b096e4fae57ce2b899472ed89d72b36c0a50d
chenpengxuan <18602201@qq.com>
2017-04-06 15:02:18 +0800

    增加纯缓存数据查询接口

--- facade/model/src/main/java/com/ymatou/productprice/model/req/GetPriceCacheRequest.java
+++ facade/model/src/main/java/com/ymatou/productprice/model/req/GetPriceCacheRequest.java
@@ -1,7 +1,9 @@
 package com.ymatou.productprice.model.req;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.QueryParam;
 import java.util.List;
 
 /**
@@ -11,14 +13,16 @@ public class GetPriceCacheRequest extends BaseRequest{
     /**
      * 商品id
      */
-    @QueryParam("productIdList")
+    @JsonProperty("ProductIds")
     @NotNull(message = "商品id列表不能为空")
     private List<String> productIdList;
 
+    @JsonIgnore
     public List<String> getProductIdList() {
         return productIdList;
     }
 
+    @JsonIgnore
     public void setProductIdList(List<String> productIdList) {
         this.productIdList = productIdList;
     }
