831f1ebae100fe96f77d56ab6d783a420fcba29f
chenpengxuan <18602201@qq.com>
2017-04-17 20:16:14 +0800

    把压测注释掉的监控代码进行还原

--- infrastructure/src/main/java/com/ymatou/productprice/infrastructure/config/PerformanceConfig.java
+++ infrastructure/src/main/java/com/ymatou/productprice/infrastructure/config/PerformanceConfig.java
@@ -7,8 +7,13 @@
 
 package com.ymatou.productprice.infrastructure.config;
 
+import com.ymatou.performancemonitorclient.PerformanceMonitorAdvice;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -23,42 +28,42 @@ public class PerformanceConfig {
     @Value("${performance.server.url}")
     private String performanceServerUrl;
 
-//    @Bean(name = "performanceMonitorAdvice")
-//    public PerformanceMonitorAdvice performanceMonitorAdvice() {
-//        PerformanceMonitorAdvice performanceMonitorAdvice = new PerformanceMonitorAdvice();
-//        performanceMonitorAdvice.setAppId("productprice.iapi.ymatou.com");
-//        performanceMonitorAdvice.setServerUrl(String.format("http://%s/api/perfmon", performanceServerUrl));
-//        return performanceMonitorAdvice;
-//    }
-//
-//    @Bean(name = "performancePointcut")
-//    public AspectJExpressionPointcut aspectJExpressionPointcut() {
-//        AspectJExpressionPointcut aspectJExpressionPointcut = new AspectJExpressionPointcut();
-//
-//        aspectJExpressionPointcut.setExpression(
-//                "execution(* com.ymatou.productprice.facade.*Facade.*(..))"
-//                        + "|| execution(* com.ymatou.productprice.domain.repo.*.*(..))"
-//                        + "|| execution(* com.ymatou.productprice.domain.service.PriceQueryService.*(..))"
-//                        + "|| execution(* com.ymatou.productprice.facade.ProductPriceFacade.*(..))"
-//                        + "|| execution(* com.ymatou.productprice.intergration.client.UserBehaviorAnalysisService.*(..))"
-//        );
-//
-//        return aspectJExpressionPointcut;
-//    }
-//
-//
-//    /**
-//     * 对应xml
-//     * <aop:config>
-//     * <aop:advisor advice-ref="performanceMonitorAdvice"
-//     * pointcut-ref="performancePointcut" />
-//     * </aop:config>
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public Advisor performanceMonitorAdvisor() {
-//        return new DefaultPointcutAdvisor(aspectJExpressionPointcut(), performanceMonitorAdvice());
-//    }
+    @Bean(name = "performanceMonitorAdvice")
+    public PerformanceMonitorAdvice performanceMonitorAdvice() {
+        PerformanceMonitorAdvice performanceMonitorAdvice = new PerformanceMonitorAdvice();
+        performanceMonitorAdvice.setAppId("productprice.iapi.ymatou.com");
+        performanceMonitorAdvice.setServerUrl(String.format("http://%s/api/perfmon", performanceServerUrl));
+        return performanceMonitorAdvice;
+    }
+
+    @Bean(name = "performancePointcut")
+    public AspectJExpressionPointcut aspectJExpressionPointcut() {
+        AspectJExpressionPointcut aspectJExpressionPointcut = new AspectJExpressionPointcut();
+
+        aspectJExpressionPointcut.setExpression(
+                "execution(* com.ymatou.productprice.facade.*Facade.*(..))"
+                        + "|| execution(* com.ymatou.productprice.domain.repo.*.*(..))"
+                        + "|| execution(* com.ymatou.productprice.domain.service.PriceQueryService.*(..))"
+                        + "|| execution(* com.ymatou.productprice.facade.ProductPriceFacade.*(..))"
+                        + "|| execution(* com.ymatou.productprice.intergration.client.UserBehaviorAnalysisService.*(..))"
+        );
+
+        return aspectJExpressionPointcut;
+    }
+
+
+    /**
+     * 对应xml
+     * <aop:config>
+     * <aop:advisor advice-ref="performanceMonitorAdvice"
+     * pointcut-ref="performancePointcut" />
+     * </aop:config>
+     *
+     * @return
+     */
+    @Bean
+    public Advisor performanceMonitorAdvisor() {
+        return new DefaultPointcutAdvisor(aspectJExpressionPointcut(), performanceMonitorAdvice());
+    }
 
 }
